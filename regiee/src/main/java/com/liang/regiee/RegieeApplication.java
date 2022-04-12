a2ae745cbd9d855d9a94ed6dc9f7f5644eae2f12
M Love K <mlovek@MdeMacBook-Pro.local>
2022-04-12 22:48:49 +0800

    the_1

---
+++ regiee/src/main/java/com/liang/regiee/RegieeApplication.java
@@ -0,0 +1,14 @@
+package com.liang.regiee;
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegieeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegieeApplication.class, args);
+    }
+
+}
---
+++ regiee/src/test/java/com/liang/regiee/RegieeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liang.regiee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegieeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
