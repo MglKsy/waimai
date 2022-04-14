535d7409cfd78c6cbd447288e2548251f8d2a88b
mgl <2529947582@qq.com>
2022-04-14 15:07:04 +0800

    the_6

---
+++ regiee/src/main/java/com/liang/regiee/common/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.liang.regiee.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@ControllerAdvice(annotations = {RestController.class, Controller.class})
+@Slf4j
+@ResponseBody
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> SQLIntegrityConstraintViolationExceptionHandler(SQLIntegrityConstraintViolationException e){
+        log.info("错误信息:{}",e.getMessage());
+        if (e.getMessage().contains("Duplicate")){
+            String[] strings = e.getMessage().split(" ");
+            String msg = strings[2] + "已存在";
+            return R.error(msg);
+        }
+        
+        return R.error("未知错误");
+    }
+}
